1e48788e2caf2bf8dde505e272d68afcfe4fd4b2
LiuYuancheng <liu_yuan_cheng@hotmail.com>
2017-09-26 18:35:27 +0800

    Update ass1.c

--- ass1.c
+++ ass1.c
@@ -1,57 +1,49 @@
+
 #include <stdio.h>
 #include <time.h>
 #include <math.h>
 #include <stdlib.h>
- 
-int main()
+
+int main(int argc, char *argv[])
 {
-  int n,  q, c, d, k, sum = 0; 
+  int n,  q, c, d, k, sum = 0;
   printf("Enter the number n for the nxn matrix\n");
-  scanf("%d", &n);
+  //scanf("%d", &n);
+  n = atoi(argv[1]);
   printf("Enter the elements of first matrix\n");
-	int first[n][n], second[n][n], multiply[n][n];
-  // init the first matrix
-	for (c = 0; c < n; c++)
+        int first[n][n], second[n][n], multiply[n][n];
+
+  for (c = 0; c < n; c++)
     for (d = 0; d < n; d++)
-      first[c][d] = rand() % 10; 
-  // init the second matrix 
+      first[c][d] = rand() % 10;
+
    for (c = 0; c < n; c++)
      for (d = 0; d < n; d++)
-       second[c][d]= rand() % 10;  
-  //Calculation [x]x[x]
+       second[c][d]= rand() % 10;
 
-	struct timespec startW, endW;  
-	
-	// wall clock time
-	clock_gettime(CLOCK_REALTIME, &startW); 
-	// cpu time 	
-	double start = (double)clock() /(double) CLOCKS_PER_SEC;
-
-    for (c = 0; c < n; c++) {
+  struct timespec startW, endW;
+  clock_gettime(CLOCK_REALTIME, &startW);
+  double start = (double)clock() /(double) CLOCKS_PER_SEC;
+  for (c = 0; c < n; c++) {
       for (d = 0; d < n; d++) {
         for (k = 0; k < n; k++) {
           sum = sum + first[c][k]*second[k][d];
         }
- 
+
         multiply[c][d] = sum;
         sum = 0;
       }
     }
- 
-	double end = (double)clock() / (double) CLOCKS_PER_SEC;
-	// 
-	clock_gettime(CLOCK_REALTIME, &endW);
-	double seconds = (double)((endW.tv_sec+endW.tv_nsec*1e-9) - (double)(startW.tv_sec+startW.tv_nsec*1e-9));
-	printf("wall time %fs\n", seconds);
-	printf("cpu time %fs\n", end - start);
+  double end = (double)clock() / (double) CLOCKS_PER_SEC;
+  clock_gettime(CLOCK_REALTIME, &endW);
+  double seconds = (double)((endW.tv_sec+endW.tv_nsec*1e-9) - (double)(startW.tv_sec+startW.tv_nsec*1e-9));
+  printf("wall time %fs\n", seconds);
+  printf("cpu time %fs\n", end - start);
 
-
-	printf("Product of entered matrices:-\n");
+  printf("Product of entered matrices:%d-\n", n);
     for (c = 0; c < n; c++) {
       for (d = 0; d < n; d++)
-        //printf("%d\t", multiply[c][d]);
- 				multiply[c][d]=0; 
-      //printf("\n");
+        multiply[c][d]=0;
     }
   return 0;
 }
